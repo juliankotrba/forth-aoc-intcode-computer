ce41d9cacef54ff61bf582451932c5c779d2f289
Julian Kotrba <me@juliankotrba.xyz>
2019-12-09 21:33:22 +0100

    Add function for reading intcode

---
+++ input-util.fs
@@ -0,0 +1,9 @@
+create intcode 100 cells allot \ TODO: Create array in function read-intcode
+
+: read-intcode ( x1 x2 x3 xn n -- addr )
+       dup 0 ?DO
+        dup  i - rot intcode rot cells + !
+     LOOP drop intcode ;
+
+: test 
+     4 3 2 1 0 5 read-intcode ;
