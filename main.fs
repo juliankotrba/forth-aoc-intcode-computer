2e7cdaaa3d1ad321e923e23344b24a6864b94bc6
David Walter <hello@davidwalter.at>
2019-12-10 16:58:46 +0100

    Refactor main.fs
    Create test.fs

--- main.fs
+++ main.fs
@@ -1,34 +1,36 @@
 require input-util.fs
 
-: run ( count addr -- todo )
+: read-parameters
+    2dup 2dup 
+    1 + read intcode swap read -rot     \ 1. parameter
+    2 + read  intcode swap read ;       \ 2. parameter
+
+: write-result
+    -rot 
+    3 + read intcode -rot write ;
+
+: add ( count addr index -- )
+    read-parameters
+    + \ result
+    write-result ;
+
+: mul ( count addr index -- )
+    read-parameters
+    * \ result
+    write-result ;
+
+: run ( count addr -- count addr index )
+    s\" \n" type
     0
     begin
+        print s\" \n" type
         2dup read 99 <> while
             
             2dup 2dup read 1 = if  
-               2dup 2dup 
-               
-                1 + read intcode swap read -rot  \ 1. parameter
-                2 + read  intcode swap read    \ 2. parameter
-                + \ result
-                -rot 
-                3 + read intcode -rot write 
+               add
             else 
-                2dup 2dup 
-               
-                1 + read intcode swap read -rot  \ 1. parameter
-                2 + read  intcode swap read    \ 2. parameter
-                * \ result
-                -rot 
-                3 + read intcode -rot write  
+                mul
             endif 
 
         4 +
     repeat ;
-
-
-test-input-1 run print
-\ test-input-2 run print
-\ test-input-3 run print
-\ test-input-4 run print
-\ test-input-long run print-first
