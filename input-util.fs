c5ef57b948811fee74bdae4fb801491acb98a956
Julian Kotrba <me@juliankotrba.xyz>
2019-12-10 15:08:15 +0100

    Add basic loop

--- input-util.fs
+++ input-util.fs
@@ -2,11 +2,11 @@ create intcode 100 cells allot \ TODO: Create array in function read-intcode
 
 : read-intcode ( x1 x2 x3 xn n -- addr )
        dup 0 ?DO
-        dup  i - rot intcode rot cells + !
+        dup  i - 1 - rot intcode rot cells + !
      LOOP drop intcode ;
 
 : read ( addr i -- n)
      2dup cells + @ ; 
 
 : test 
-     4 3 2 1 0 5 read-intcode ;
+     1 2 99 1 99 5 read-intcode ;
---
+++ main.fs
@@ -0,0 +1,11 @@
+require input-util.fs
+
+: main
+    test 0
+    
+    begin
+        read 99 <> while
+        
+
+        4 +
+    repeat .s ;
